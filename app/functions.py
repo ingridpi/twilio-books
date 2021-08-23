2aa7bd828c0a01ebfa1f2e0e8c39119c8138eda6
ingridpi <99.perez.in@gmail.com>
2021-08-23 17:38:47 +0200

    Books-01 Check if date is null

--- app/functions.py
+++ app/functions.py
@@ -104,8 +104,10 @@ def book_info(msg, title, sender):
             rating = book["Average Rating"].values[0]
             book_info = "*{}* \nAuthor: {} \nPages: {} \nAverage Rating: {}".format(title, author, pages, rating)
 
-            date_read = book["Date Read"].dt.strftime("%d %b %Y").values[0]
-            if not np.isnan(date_read):
+            date_read = book["Date Read"].values[0]
+
+            if not np.isnat(date_read):
+                date_read = book["Date Read"].dt.strftime("%d %b %Y").values[0]
                 my_rating = book["My Rating"].values[0]
                 book_info += "\nDate Read: {} \nMy Rating: {}".format(date_read, my_rating)
 
